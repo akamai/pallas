f2703b4e44a39f253483d31d55c3aa64c4d49226
Miloslav Pojman <mpojman@akamai.com>
2020-02-14 13:05:50 +0100

    Refactor storage tests based on source chandes.

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,54 +3,57 @@ import pytest
 from pallas.storage import NotFoundError, MemoryStorage, FileStorage, S3Storage
 
 
-@pytest.fixture(name="memory_cache")
-def memory_cache_fixture(tmp_path):
+@pytest.fixture(name="memory_storage")
+def memory_storage_fixture(tmp_path):
     return MemoryStorage()
 
 
-@pytest.fixture(name="file_cache")
-def file_cache_fixture(tmp_path):
+@pytest.fixture(name="file_storage")
+def file_storage_fixture(tmp_path):
     return FileStorage(tmp_path)
 
 
-@pytest.fixture(name="s3_cache")
-def s3_cache_fixture(s3_tmp_uri):
+@pytest.fixture(name="s3_storage")
+def s3_storage_fixture(s3_tmp_uri):
     return S3Storage.from_uri(s3_tmp_uri)
 
 
-@pytest.fixture(name="cache", params=["memory", "file", "s3"])
-def cache_fixture(request):
-    yield request.getfixturevalue(f"{request.param}_cache")
+@pytest.fixture(name="storage", params=["memory", "file", "s3"])
+def storage_fixture(request):
+    """
+    Yields all implemented storage implementations.
+    """
+    yield request.getfixturevalue(f"{request.param}_storage")
 
 
-class TestCaches:
-    def test_get_missing(self, cache):
+class TestStorage:
+    def test_get_missing(self, storage):
         with pytest.raises(NotFoundError):
-            cache.get("foo")
+            storage.get("foo")
 
-    def test_set_get(self, cache):
-        cache.set("foo", "Hello Foo!")
-        assert cache.get("foo") == "Hello Foo!"
+    def test_set_get(self, storage):
+        storage.set("foo", "Hello Foo!")
+        assert storage.get("foo") == "Hello Foo!"
 
-    def test_has_missing(self, cache):
-        assert not cache.has("foo")
+    def test_has_missing(self, storage):
+        assert not storage.has("foo")
 
-    def test_has_present(self, cache):
-        cache.set("foo", "Hello Foo!")
-        assert cache.has("foo")
+    def test_has_present(self, storage):
+        storage.set("foo", "Hello Foo!")
+        assert storage.has("foo")
 
-    def test_read_missing(self, cache):
+    def test_read_missing(self, storage):
         with pytest.raises(NotFoundError):
-            cache.reader("foo")
+            storage.reader("foo")
 
-    def test_read(self, cache):
-        cache.set("foo", "Hello Foo,\nHope you are doing well.")
-        with cache.reader("foo") as stream:
+    def test_read(self, storage):
+        storage.set("foo", "Hello Foo,\nHope you are doing well.")
+        with storage.reader("foo") as stream:
             assert stream.readline() == "Hello Foo,\n"
             assert stream.readline() == "Hope you are doing well."
 
-    def test_write(self, cache):
-        with cache.writer("foo") as stream:
+    def test_write(self, storage):
+        with storage.writer("foo") as stream:
             stream.write("Hello Foo,\n")
             stream.write("Hope you are doing well.")
-        assert cache.get("foo") == "Hello Foo,\nHope you are doing well."
+        assert storage.get("foo") == "Hello Foo,\nHope you are doing well."
