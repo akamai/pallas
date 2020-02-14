bee4804741063a2915528cfed190b45a8a9eb960
Miloslav Pojman <mpojman@akamai.com>
2020-02-14 16:12:48 +0100

    Better QueryInfo.

--- src/pallas/info.py
+++ src/pallas/info.py
@@ -1,13 +1,58 @@
 from __future__ import annotations
 
+import datetime as dt
 from typing import cast, Any, Mapping, Optional
 
 from pallas.exceptions import AthenaQueryError
 
 
+unit_prefixes = ["k", "M", "G", "T"]
+
+
+def format_price(v: float) -> str:
+    if v > 1:
+        return "$%.2f" % v
+    return "%.2f¢" % (100 * v)
+
+
+def format_size(v: float) -> str:
+    if v < 1000:
+        return f"{v:.0f}B"
+    for prefix in unit_prefixes:
+        v /= 1000
+        if v < 1000:
+            break
+    return f"{v:.2f}{prefix}B"
+
+
+def format_time(v: dt.timedelta) -> str:
+    if v < dt.timedelta(minutes=1):
+        s = v.total_seconds()
+        return f"{s:.1f}s"
+    m, s = divmod(v.seconds, 60)
+    return f"{m}min {s}s"
+
+
 class QueryInfo:
+    """
+    Information about query execution.
+
+    Provides access to data returned by Athena GetQueryExecution API method.
+    """
+
     def __init__(self, data: Mapping[str, Any]):
         self._data = data
+
+    def __repr__(self):
+        return f"<{type(self).__name__}: {str(self)!r}>"
+
+    def __str__(self) -> str:
+        return (
+            f"{self.state}, "
+            f"scanned {format_size(self.scanned_bytes)} "
+            f"in {format_time(self.execution_time)}, "
+            f"approx. price {format_price(self.approx_price)}"
+        )
 
     @property
     def execution_id(self) -> str:
@@ -15,11 +60,18 @@ class QueryInfo:
 
     @property
     def sql(self) -> str:
-        return cast(str, self._data["Query"])
+        rv = self._data["Query"]
+        return cast(str, rv)
+
+    @property
+    def output_location(self) -> Optional[str]:
+        rv = self._data["ResultConfiguration"].get("OutputLocation")
+        return cast(Optional[str], rv)
 
     @property
     def database(self) -> Optional[str]:
-        return cast(Optional[str], self._data["QueryExecutionContext"].get("Database"))
+        rv = self._data["QueryExecutionContext"].get("Database")
+        return cast(Optional[str], rv)
 
     @property
     def finished(self) -> bool:
@@ -31,11 +83,28 @@ class QueryInfo:
 
     @property
     def state(self) -> str:
-        return cast(str, self._data["Status"]["State"])
+        rv = self._data["Status"]["State"]
+        return cast(str, rv)
 
     @property
     def state_reason(self) -> Optional[str]:
-        return cast(Optional[str], self._data["Status"].get("StateChangeReason"))
+        rv = self._data["Status"].get("StateChangeReason")
+        return cast(Optional[str], rv)
+
+    @property
+    def scanned_bytes(self) -> int:
+        rv = self._data["Statistics"].get("DataScannedInBytes", 0)
+        return cast(int, rv)
+
+    @property
+    def execution_time(self) -> dt.timedelta:
+        milliseconds = self._data["Statistics"].get("TotalExecutionTimeInMillis", 0)
+        return dt.timedelta(milliseconds=milliseconds)
+
+    @property
+    def approx_price(self) -> float:
+        price_per_tb = 5  # https://aws.amazon.com/athena/pricing/
+        return price_per_tb * self.scanned_bytes / 10 ** 12
 
     def check(self) -> None:
         if self.finished and not self.succeeded:
--- tests/test_info.py
+++ tests/test_info.py
@@ -1,17 +1,40 @@
 import copy
+import datetime as dt
 
 import pytest
 
 from pallas.exceptions import AthenaQueryError
-from pallas.info import QueryInfo
+from pallas.info import format_size, format_price, format_time, QueryInfo
+
+
+def test_format_price():
+    assert format_price(0.00011) == "0.01¢"
+    assert format_price(0.0011) == "0.11¢"
+    assert format_price(0.011) == "1.10¢"
+    assert format_price(0.11) == "11.00¢"
+    assert format_price(1.1) == "$1.10"
+
+
+def test_format_size():
+    assert format_size(1.1) == "1B"
+    assert format_size(1_100) == "1.10kB"
+    assert format_size(1_100_000) == "1.10MB"
+    assert format_size(1_100_000_000) == "1.10GB"
+    assert format_size(1_100_000_000_000) == "1.10TB"
+    assert format_size(1_100_000_000_000_000) == "1100.00TB"
+
+
+def test_format_time():
+    assert format_time(dt.timedelta(seconds=13, milliseconds=570)) == "13.6s"
+    assert format_time(dt.timedelta(minutes=13, seconds=57)) == "13min 57s"
 
 
 EXAMPLE_INFO = {
-    "QueryExecutionId": "e836081e-77f3-5700-4cdb-df1ba5799046",
-    "Query": "SELECT 1",
+    "QueryExecutionId": "e83608...1e",
+    "Query": "SELECT ...",
     "StatementType": "DML",
     "ResultConfiguration": {
-        "OutputLocation": "s3://example-output-location/e836081e-77f3-5700-4cdb-df1ba5799046.csv"  # noqa: E501
+        "OutputLocation": "s3://example-output-location/e83608...1e.csv"
     },
     "QueryExecutionContext": {"Database": "example-database"},
     "Status": {
@@ -21,7 +44,7 @@ EXAMPLE_INFO = {
     },
     "Statistics": {
         "EngineExecutionTimeInMillis": 300,
-        "DataScannedInBytes": 0,
+        "DataScannedInBytes": 10 ** 9,
         "TotalExecutionTimeInMillis": 400,
         "QueryQueueTimeInMillis": 50,
         "ServiceProcessingTimeInMillis": 20,
@@ -40,12 +63,25 @@ def get_info(*, state=None, state_reason=None):
 
 
 class TestQueryInfo:
+    def test_repr(self):
+        info = get_info()
+        assert repr(info) == (
+            "<QueryInfo: 'SUCCEEDED, scanned 1.00GB in 0.4s, approx. price 0.50¢'>"
+        )
+
+    def test_str(self):
+        info = get_info()
+        assert str(info) == "SUCCEEDED, scanned 1.00GB in 0.4s, approx. price 0.50¢"
 
     def test_properties(self):
         info = get_info()
-        assert info.execution_id == "e836081e-77f3-5700-4cdb-df1ba5799046"
-        assert info.sql == "SELECT 1"
+        assert info.execution_id == "e83608...1e"
+        assert info.sql == "SELECT ..."
         assert info.database == "example-database"
+        assert info.output_location == "s3://example-output-location/e83608...1e.csv"
+        assert info.scanned_bytes == 1_000_000_000
+        assert info.execution_time == dt.timedelta(seconds=0.4)
+        assert info.approx_price == 0.005
 
     @pytest.mark.parametrize("state", ["QUEUED", "RUNNING"])
     def test_running(self, state):
