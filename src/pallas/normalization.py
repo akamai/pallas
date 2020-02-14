64cc7638c82f99a7cf2bbffedd5a86cc5560f8c1
Miloslav Pojman <mpojman@akamai.com>
2020-02-14 17:43:28 +0100

    Implement query normalization.

---
+++ src/pallas/normalization.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import textwrap
+from typing import Optional
+
+from pallas.base import Athena, Query
+
+
+def normalize_sql(sql: str) -> str:
+    lines = sql.splitlines()
+    stripped = (l.rstrip() for l in lines)
+    nonempty = (l for l in stripped if l)
+    joined = "\n".join(nonempty)
+    return textwrap.dedent(joined)
+
+
+class AthenaNormalizationWrapper:
+    """
+    Athena wrapper that normalizes executed queries.
+
+    Query normalization can improve caching.
+
+    Following normalization operations are done:
+    - Trailing whitespace is removed from end of lines.
+    - Empty lines and lines with whitespace only are removed.
+    - Common indentation is removed.
+    - Line endings are normalized to LF
+    """
+
+    _inner_athena: Athena
+
+    def __init__(self, athena: Athena) -> None:
+        self._inner_athena = athena
+
+    @property
+    def database(self) -> Optional[str]:
+        return self._inner_athena.database
+
+    def submit(self, sql: str) -> Query:
+        normalized = normalize_sql(sql)
+        return self._inner_athena.submit(normalized)
+
+    def get_query(self, execution_id: str) -> Query:
+        return self._inner_athena.get_query(execution_id)
---
+++ tests/test_normalization.py
@@ -0,0 +1,81 @@
+import pytest
+
+from pallas.normalization import AthenaNormalizationWrapper, normalize_sql
+from pallas.testing import AthenaFake
+
+NORMALIZED_SQL = """\
+SELECT
+    c1, c2
+FROM
+    t\
+"""
+
+
+class TestNormalizeSQL:
+    def test_dedent(self):
+        sql = normalize_sql(
+            """
+            SELECT
+                c1, c2
+            FROM
+                t
+        """
+        )
+        assert sql == NORMALIZED_SQL
+
+    def test_trailing_whitespace(self):
+        sql = normalize_sql(
+            """\N{space}
+            SELECT\N{space}\N{space}\N{space}
+                c1, c2\N{space}
+            FROM\N{space}
+                t\N{space}
+        """
+        )
+        assert sql == NORMALIZED_SQL
+
+    def test_empty_lines(self):
+        sql = normalize_sql(
+            """\n
+            SELECT\n\n\n
+                c1, c2\n
+            FROM\n
+                t\n
+        """
+        )
+        assert sql == NORMALIZED_SQL
+
+
+@pytest.fixture(name="athena")
+def athena_fixture():
+    fake = AthenaFake()
+    fake.database = "test_database"
+    return AthenaNormalizationWrapper(fake)
+
+
+class TestAthenaNormalizationWrapper:
+    def test_database(self, athena):
+        assert athena.database == "test_database"
+
+    def test_submit(self, athena):
+        query = athena.submit(
+            """
+            SELECT
+                c1, c2
+            FROM
+                t
+        """
+        )
+        assert query.get_info().sql == NORMALIZED_SQL
+
+    def test_get_query(self, athena):
+        athena.submit(
+            """
+            SELECT
+                c1, c2
+            FROM
+                t
+        """
+        )
+        query = athena.get_query("query-1")
+        assert query.get_info().sql == NORMALIZED_SQL
