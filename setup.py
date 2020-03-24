fb86a65a2ff9352e21d75bf8a1b026276d3fc717
Miloslav Pojman <mpojman@akamai.com>
2020-03-24 17:54:02 +0100

    Relase 0.1.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="pallas",
-    version="0.1rc1",
+    version="0.1",
     author="Miloslav Pojman",
     author_email="mpojman@akamai.com",
     description="AWS Athena client",
