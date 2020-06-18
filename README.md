042ccf74041d37d746f7b8b348cb7189a90c66a8
Miloslav Pojman <mpojman@akamai.com>
2020-06-18 16:43:02 +0200

    Increment version after the release.

--- README.md
+++ README.md
@@ -184,6 +184,9 @@ $ tox
 
 ## Changelog
 
+### v0.4.dev
+* ...
+
 ### v0.3 (2020-06-18)
 
 * Athena and Query classes available from the top-level module (useful for type hints).
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="pallas",
-    version="0.3",
+    version="0.4.dev",
     author="Miloslav Pojman",
     author_email="mpojman@akamai.com",
     description="AWS Athena client",
