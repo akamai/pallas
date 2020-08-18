caa39be24b0e1e0b8325e838854b515c549fbb4f
Miloslav Pojman <mpojman@akamai.com>
2020-08-18 18:22:14 +0200

    Increment version after a release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.rst", "r") as fp:
 
 setup(
     name="pallas",
-    version="0.4",
+    version="0.5.dev",
     author="Miloslav Pojman",
     author_email="mpojman@akamai.com",
     description="AWS Athena client",
