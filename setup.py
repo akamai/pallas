5222532f90370cb6adcadf71399d181e973f7c91
Miloslav Pojman <mpojman@akamai.com>
2021-03-30 11:50:13 +0200

    Release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.rst", "r") as fp:
 
 setup(
     name="pallas",
-    version="0.9.dev",
+    version="0.9",
     author="Miloslav Pojman",
     author_email="mpojman@akamai.com",
     description="AWS Athena client",
